f7926fea308c567ac4b9f641f1dec79c2d08f2d4
isabellybecker <159549252+isabellybecker@users.noreply.github.com>
2024-10-24 11:28:14 -0300

    Add files via upload

---
+++ flashcard-main/perguntas.js
@@ -0,0 +1,95 @@
+criaCartao( 
+    'Filmes', 
+    'Qual é o filme mais lucrativo de todos os tempos?', 
+    'O filme mais lucrativo de todos os tempos é "Avatar".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Quem dirigiu "O Senhor dos Anéis"?', 
+    'Peter Jackson dirigiu "O Senhor dos Anéis".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual filme ganhou o Oscar de Melhor Filme em 2020?', 
+    'O filme que ganhou o Oscar de Melhor Filme em 2020 foi "Parasita".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Quem é o protagonista de "Piratas do Caribe"?', 
+    'O protagonista de "Piratas do Caribe" é Jack Sparrow, interpretado por Johnny Depp.' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é a animação mais famosa da Disney?', 
+    'Uma das animações mais famosas da Disney é "O Rei Leão".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Em que ano foi lançado "Titanic"?', 
+    'O filme "Titanic" foi lançado em 1997.' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Quem interpretou o Coringa em "O Cavaleiro das Trevas"?', 
+    'Heath Ledger interpretou o Coringa em "O Cavaleiro das Trevas".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é o filme de maior bilheteira de 2021?', 
+    'O filme de maior bilheteira de 2021 foi "Spider-Man: No Way Home".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é o nome do famoso espião britânico interpretado por Daniel Craig?', 
+    'O famoso espião britânico é James Bond.' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual filme é conhecido pela frase "Eu sou seu pai"?', 
+    'A frase "Eu sou seu pai" é famosa no filme "Star Wars: Episódio V - O Império Contra-Ataca".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Quem é a diretora de "Lady Bird"?', 
+    'Greta Gerwig é a diretora de "Lady Bird".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual filme apresenta a história de um menino que não quer crescer?', 
+    'O filme que apresenta essa história é "Peter Pan".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é o nome do famoso filme de terror dirigido por Alfred Hitchcock?', 
+    'O famoso filme de terror dirigido por Alfred Hitchcock é "Psicose".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Quem interpretou a personagem de Hermione Granger na série "Harry Potter"?', 
+    'Emma Watson interpretou Hermione Granger.' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é o nome do filme que conta a história de um robô chamado Wall-E?', 
+    'O nome do filme é "Wall-E".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual filme de animação ganhou o Oscar de Melhor Filme?', 
+    'O filme de animação que ganhou o Oscar de Melhor Filme foi "A Viagem de Chihiro".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é o nome do filme que se passa em um mundo pós-apocalíptico e apresenta zumbis?', 
+    'O filme é "Madrugada dos Mortos".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Qual é o nome do famoso filme de ficção científica dirigido por Stanley Kubrick?', 
+    'O filme é "2001: Uma Odisseia no Espaço".' 
+) 
+criaCartao( 
+    'Filmes', 
+    'Quem é o ator principal em "O Lobo de Wall Street"?', 
+    'Leonardo DiCaprio é o ator principal em "O Lobo de Wall Street".' 
+) 
